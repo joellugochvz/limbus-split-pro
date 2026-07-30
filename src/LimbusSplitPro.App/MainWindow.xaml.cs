e7d324538625c48a1027b80e5d7f18c098329939
joellugochvz <joel.eduardo.lugo.chavez@gmail.com>
2026-07-30 14:47:32 -0600

    UI: liberar el mixer al cerrar la ventana

--- src/LimbusSplitPro.App/MainWindow.xaml.cs
+++ src/LimbusSplitPro.App/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        Closed += (_, _) => (DataContext as IDisposable)?.Dispose();
     }
 
     /// <summary>
